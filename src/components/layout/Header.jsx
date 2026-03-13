8d76fb107fb0be1799f2424ea1a77f89e0d8067b
BlesUstetu <lionustetu@gmail.com>
2026-03-13 12:29:13 +0100

    Update Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,16 +2,24 @@ import { Link } from "react-router-dom"
 import ConnectWallet from "../wallet/ConnectWallet"
 
 export default function Header(){
-  return(
-    <header className="header">
-      <div className="logo">HybridDEX</div>
-      <nav>
-        <Link to="/">Dashboard</Link>
-        <Link to="/trade">Trade</Link>
-        <Link to="/swap">Swap</Link>
-        <Link to="/vault">Vault</Link>
-      </nav>
-      <ConnectWallet/>
-    </header>
-  )
+
+return(
+<header className="header">
+ <div className="logo">
+  HybridDEX
+ </div>
+  <nav>
+    <Link to="/">Dashboard</Link>
+    <Link to="/trade">Trade</Link>
+    <Link to="/futures">Futures</Link>
+    <Link to="/swap">Swap</Link>
+    <Link to="/vault">Vault</Link>
+    <Link to="/markets">Markets</Link>
+    <Link to="/portfolio">Portfolio</Link>
+  </nav>
+  <ConnectWallet/>
+ </header>
+
+)
+
 }
