84655ad2cc53614bcb35b779ea19c344bf63b807
BlesUstetu <lionustetu@gmail.com>
2026-03-14 08:06:17 +0100

    Update SpotTrade.jsx

--- src/pages/SpotTrade.jsx
+++ src/pages/SpotTrade.jsx
@@ -1,37 +1,12 @@
-import { useState } from "react"
-
-import MarketSelector from "../components/trading/MarketSelector"
-import OrderBook from "../components/trading/OrderBook"
-import TradingChart from "../components/trading/TradingChart"
-import TradeForm from "../components/trading/TradeForm"
-import TradeHistory from "../components/trading/TradeHistory"
-
 export default function SpotTrade(){
-
-const [market,setMarket]=useState("BTC/USDT")
 
 return(
 
-<div>
+<div className="page">
 
-<h2>{market}</h2>
+<h1>Spot Trading</h1>
 
-<MarketSelector
-market={market}
-setMarket={setMarket}
-/>
-
-<div className="trade-layout">
-
-<OrderBook/>
-
-<TradingChart/>
-
-<TradeForm/>
-
-</div>
-
-<TradeHistory/>
+<p>Trading interface coming soon</p>
 
 </div>
 
