b71d359d0252694dbc181a3d068f5c0de4eb87a8
BlesUstetu <lionustetu@gmail.com>
2026-03-14 11:39:53 +0100

    Update OrderBook.jsx

--- src/components/trading/OrderBook.jsx
+++ src/components/trading/OrderBook.jsx
@@ -1,48 +1,63 @@
-import { useEffect,useState } from "react"
+import { useMarketStore } from "../../core/store"
 
 export default function OrderBook(){
 
-const [bids,setBids]=useState([])
-const [asks,setAsks]=useState([])
-
-useEffect(()=>{
-
-const ws = new WebSocket(
-"wss://stream.binance.com:9443/ws/btcusdt@depth"
-)
-
-ws.onmessage=(event)=>{
-
-const data = JSON.parse(event.data)
-
-setBids(data.b || [])
-setAsks(data.a || [])
-
-}
-
-return ()=> ws.close()
-
-},[])
+const bids = useMarketStore(s=>s.bids)
+const asks = useMarketStore(s=>s.asks)
 
 return(
 
 <div className="panel">
 
-<h3>OrderBook</h3>
+<h3>Order Book</h3>
 
 <div>
 
-{asks.slice(0,8).map((a,i)=>(
-<div key={i}>{a[0]} | {a[1]}</div>
-))}
+{asks.slice(0,12).map((a,i)=>{
+
+const vol = Number(a[1])
+
+return(
+
+<div
+key={i}
+style={{
+background:`rgba(239,68,68,${Math.min(vol/5,0.7)})`
+}}
+>
+
+{a[0]} | {a[1]}
+
+</div>
+
+)
+
+})}
 
 </div>
 
 <div>
 
-{bids.slice(0,8).map((b,i)=>(
-<div key={i}>{b[0]} | {b[1]}</div>
-))}
+{bids.slice(0,12).map((b,i)=>{
+
+const vol = Number(b[1])
+
+return(
+
+<div
+key={i}
+style={{
+background:`rgba(34,197,94,${Math.min(vol/5,0.7)})`
+}}
+>
+
+{b[0]} | {b[1]}
+
+</div>
+
+)
+
+})}
 
 </div>
 
