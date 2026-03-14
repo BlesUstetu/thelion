eadbb5ebc936cae2b1115e85fdf7bc3eb10e3712
BlesUstetu <lionustetu@gmail.com>
2026-03-14 10:50:45 +0100

    Create Web3Background.jsx

---
+++ src/components/layout/Web3Background.jsx
@@ -0,0 +1,9 @@
+export default function Web3Background(){
+return(
+<div className="web3-bg">
+ <div className="orb orb1"></div>
+ <div className="orb orb2"></div>
+ <div className="orb orb3"></div>
+</div>
+)
+}
