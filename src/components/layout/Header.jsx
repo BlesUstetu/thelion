6a146953538d01b1677e0e068c10567ab91d160a
BlesUstetu <lionustetu@gmail.com>
2026-03-14 08:49:09 +0100

    Update Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,31 +1,27 @@
 import { Link } from "react-router-dom"
-import WalletButton from "../wallet/WalletButton"
+import WalletConnect from "../wallet/WalletConnect"
 
 export default function Header(){
 
 return(
 
-<header className="header">
+<div className="header">
 
 <div className="logo">
 Thelion
 </div>
 
-<nav>
+<div className="menu">
 
 <Link to="/">Dashboard</Link>
+<Link to="/spot">Spot</Link>
+<Link to="/futures">Futures</Link>
 
-<Link to="/trade">Trade</Link>
+</div>
 
-<Link to="/swap">Swap</Link>
+<WalletConnect/>
 
-<Link to="/vault">Vault</Link>
-
-</nav>
-
-<WalletButton/>
-
-</header>
+</div>
 
 )
 
