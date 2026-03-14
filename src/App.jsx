b26b25407f6e86fec53eb8185c4c69732c7b6570
BlesUstetu <lionustetu@gmail.com>
2026-03-14 07:29:56 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-
 import Header from "./components/layout/Header"
-
 import Dashboard from "./pages/Dashboard"
 import SpotTrade from "./pages/SpotTrade"
 import Swap from "./pages/Swap"
 import Vault from "./pages/Vault"
 
 export default function App(){
-
 return(
-
 <BrowserRouter>
-
-<Header/>
-
-<Routes>
-
-<Route path="/" element={<Dashboard />} />
-
-<Route path="/trade" element={<SpotTrade />} />
-
-<Route path="/swap" element={<Swap />} />
-
-<Route path="/vault" element={<Vault />} />
-
-</Routes>
-
+ <Header/>
+  <Routes>
+    <Route path="/" element={<Dashboard />} />
+    <Route path="/trade" element={<SpotTrade />} />
+    <Route path="/swap" element={<Swap />} />
+    <Route path="/vault" element={<Vault />} />
+  </Routes>
 </BrowserRouter>
-
 )
-
 }
