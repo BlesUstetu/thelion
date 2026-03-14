70e669d5ae62fac1bd9fec6ffb2bc9ec5ee531ab
BlesUstetu <lionustetu@gmail.com>
2026-03-14 08:06:50 +0100

    Update Swap.jsx

--- src/pages/Swap.jsx
+++ src/pages/Swap.jsx
@@ -1,9 +1,15 @@
-import SwapCard from "../components/swap/SwapCard"
-
 export default function Swap(){
-  return(
-    <div className="page">
-      <SwapCard/>
-    </div>
-  )
+
+return(
+
+<div className="page">
+
+<h1>Swap</h1>
+
+<p>Token swap interface</p>
+
+</div>
+
+)
+
 }
